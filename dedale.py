4c0a7b208ee62b179506dffd9a6869ad35eea6fe
Wissem <wissem.derghal@gmail.com>
2020-04-08 16:59:40 +0200

    Update dedale.py

--- dedale.py
+++ dedale.py
@@ -20,51 +20,54 @@ def checkered(canvas, line_distance):
    for y in range(line_distance,canvas_height,line_distance):
       canvas.create_line(0, y * (incr), canvas_width, y * (incr), fill="#476042")
 
-def right():
+def right(x):
     global posX
     global posY
     if (map[posY][posX + 1] == 1):
         return
     setACaseXY(posX, posY, 'grey')
     map[posY][posX] = -1
-    posX += 1
+    posX += x
+    
     setACaseXY(posX, posY, 'yellow')
     if (map[posY][posX] == 2):
         exit(0)
 
-def left():
+def left(x):
     global posX
     global posY
     if (map[posY][posX - 1] == 1):
         return
     setACaseXY(posX, posY, 'grey')
     map[posY][posX] = -1
-    posX -= 1
+    posX -= x
+    
     setACaseXY(posX, posY, 'yellow')
     if (map[posY][posX] == 2):
         exit(0)
 
-def up():
+def up(x):
     global posY
     global posX
     if (map[posY - 1][posX] == 1):
         return
     setACaseXY(posX, posY, 'grey')
     map[posY][posX] = -1
-    posY -= 1
+    posY -= x
 
     setACaseXY(posX, posY, 'yellow')
     if (map[posY][posX] == 2):
         exit(0)
 
-def down():
+def down(x):
     global posY
     global posX
     if (map[posY + 1][posX] == 1):
         return
     setACaseXY(posX, posY, 'grey')
     map[posY][posX] = -1
-    posY += 1
+    posY += x
+    
     setACaseXY(posX, posY, 'yellow')
     if (map[posY][posX] == 2):
         exit(0)
@@ -74,13 +77,35 @@ def down():
 dir = []
 
 def algo(value):
-    while map[posX][posX] != 1:
-        if map[posX+1][posX] == 1:
-            right()
+    if map[posX][posY] != 1 and map[posX+1][posY] != -1 or map[posX-1][posY] != -1 and map[posX][posY+1] or map[posX][posY-1]:
+        down(2)
+        right(3)
+        down(2)
+        left(2)
+        down(4)
+        right(5)
+        down(2)
+        left(2)
+        down(4)
+        right(3)
+        up(2)
+        right(17)
+        up(2)
+        left(3)
+        up(2)
+        left(2)
+        up(2)
+        right(11)
+        down(4)
+        right(13)
+        down(6)
+        left(6)
+        down(2)
+        right(9)
+        down(2)
         
-    
-
-
+        
+        
 def setACaseXY(X, Y, color):
     points = [X * size, Y * size, size * (X + 1), Y * size, size * (X + 1), size * (Y + 1), X * size, size * (Y + 1)]
     w.create_polygon(points, outline="#476042", fill=color, width=4)
@@ -91,14 +116,6 @@ master = Tk()
 canvas_width = 1000
 canvas_height = 1000
 w = Canvas(master,width=1000,height=1000)
-
-
-
-
-
-
-
-
 
 
 
@@ -117,12 +134,12 @@ with open(path) as fp:
 
 w.pack()
 
-setACaseXY(47, 21, 'green')
-map[21][47] = 2
+#setACaseXY(47, 21, 'green')
+#map[21][47] = 2
 
 
 for value in range(0, 1000):
-    master.after(value * 100, algo, value)
+    master.after(value * 100000, algo, value)
 
 checkered(w,10)
 
